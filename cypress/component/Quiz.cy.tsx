72ebb7caf2d498f4980548e23a288698b35ce3d7
theon87 <theonfrancis17@gmail.com>
2024-11-04 23:18:36 -0500

    add cypress test for quiz component

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import Quiz from "../../client/src/components/Quiz";
 
 describe("Quiz", () => {
-  it("renders a quiz", () => {
+  it("should display a start button on load", () => {
     cy.mount(<Quiz />);
-    cy.get("h1").should("have.text", "Quiz");
-  });
+    cy.get("button").should("have.text", "Start Quiz");
+  })
+    
 });
